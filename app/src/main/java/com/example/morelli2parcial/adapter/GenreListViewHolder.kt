07ed427f7a43e1e11038f671f786a37318fb7d6d
Florencia <florenciamorelliIT@gmail.com>
2024-06-20 17:08:04 -0300

    now the selected genre opens activity_books

--- app/src/main/java/com/example/morelli2parcial/adapter/GenreListViewHolder.kt
+++ app/src/main/java/com/example/morelli2parcial/adapter/GenreListViewHolder.kt
@@ -1,7 +1,9 @@
 package com.example.morelli2parcial.adapter
 
+import android.content.Intent
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.morelli2parcial.BooksActivity
 import com.example.morelli2parcial.data.Genre
 import com.example.morelli2parcial.databinding.GenreItemBinding
 
@@ -11,5 +13,12 @@ class GenreListViewHolder(view: View) : RecyclerView.ViewHolder(view){
 
     fun bind(genre: Genre){
         binding.tvGenreName.text = genre.name
+        binding.btnGenre.setOnClickListener{
+            val intent = Intent(itemView.context, BooksActivity::class.java)
+            intent.putExtra("genre", genre.name)
+            itemView.context.startActivity(intent)
+        }
     }
+
+
 }
