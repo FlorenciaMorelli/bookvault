0e35a32a9e210a5758fa4c666708e7e03af0c056
Florencia <florenciamorelliIT@gmail.com>
2024-07-22 15:51:54 -0300

    deleted unused imports

--- app/src/main/java/com/example/morelli2parcial/BookDetailActivity.kt
+++ app/src/main/java/com/example/morelli2parcial/BookDetailActivity.kt
@@ -2,10 +2,7 @@ package com.example.morelli2parcial
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.morelli2parcial.data.Book
 
 class BookDetailActivity : AppCompatActivity() {
--- app/src/main/java/com/example/morelli2parcial/viewmodel/BookListViewModel.kt
+++ app/src/main/java/com/example/morelli2parcial/viewmodel/BookListViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.morelli2parcial.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
